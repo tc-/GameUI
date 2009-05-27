f5effdb81c802b9579b0ce0b0ebfc37c6849f284
tc- <tommyc@lavabit.com>
2009-05-27 07:47:20 +0200

    Added new widget Ui::SpriteView.

---
+++ src/uispriteview.h
@@ -0,0 +1,44 @@
+/******************************************************************************
+ * Copyright (C) 2005-2009 by Tommy Carlsson
+ *
+ * This file is part of GameUI.
+ *
+ * GameUI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * GameUI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with GameUI.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
+
+/**
+@file
+Headerfile for the Ui::SpriteView class
+*/
+
+#ifndef UISPRITEVIEW_H
+#define UISPRITEVIEW_H
+
+#include <gameui.h>
+
+#include <string>
+#include <sigslot.h>
+
+
+using std::string;
+using namespace sigslot;
+
+namespace Ui
+{
+
+}
+
+#endif
