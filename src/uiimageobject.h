ebde70e4a2d63fca491d6e0a0a16636ff8007492
Tommy Carlsson <tommyc@lavabit.com>
2009-05-30 14:42:36 +0200

    Started documenting the ImageObject class.

--- src/uiimageobject.h
+++ src/uiimageobject.h
@@ -34,122 +34,158 @@ Headerfile for the UiImageObject class
 #include <uifont.h>
 #include <cmath>
 
-namespace Ui {
+namespace Ui
+{
 
-class ImageObject;
+	class ImageObject;
 
-typedef List<ImageObject*> ImageList;
+	typedef List<ImageObject*> ImageList;
 
-typedef ImageObject* (*ImageObjectFactory)( const int&, const int& );
+	typedef ImageObject* ( *ImageObjectFactory )( const int&, const int& );
 
-/**
-Abstract class used to represent an image
-
-@author Tommy Carlsson
-*/
-class ImageObject{
-public:
-  ImageObject(  ) {}
-  virtual ~ImageObject(  ) {}
-
-  virtual void drawImage ( ImageObject &image, const Rect &dr, const Rect &sr = NULL_RECT ) = 0;
-  virtual void drawImageTiled ( ImageObject &image, const Rect &dr, const Rect &sr = NULL_RECT ) = 0;
-  virtual void drawImageStreched ( ImageObject &image, const Rect &dr, const Rect &sr = NULL_RECT ) = 0;
-
-  virtual void fillRect ( const Rect &r, const Color &color ) = 0;
-
-  virtual void outText ( const wstring &text, Font &font, int left, int top, const Color &color ) = 0;
-
-  virtual void putPixel ( int left, int top, const Color &color ) = 0;
-	virtual Color getPixel( const int& x, const int& y ) = 0;
-  virtual void hLine ( int left, int top, int width, const Color &color ) = 0;
-  virtual void vLine ( int left, int top, int height, const Color &color ) = 0;
-
-	//TODO: remove/move?
-	// Return integer part of val
-	// Chille OK
-	double ipart(double val)
+	/**
+	 * Abstract class used to represent an image or screen surface.
+	 */
+	class ImageObject
 	{
-		int tmp = (int)val;
-		return (double)tmp;
-	}
+		public:
+			ImageObject(  ) {}
+			virtual ~ImageObject(  ) {}
 
-	// Return fractional part of val
-	// Is this one correct?
-	double fpart(double val)
-	{
-		int tmp = (int)val;
-		return (val-(double)tmp);
-	}
+			virtual void drawImage ( ImageObject &image, const Rect &dr, const Rect &sr = NULL_RECT ) = 0;
+			virtual void drawImageTiled ( ImageObject &image, const Rect &dr, const Rect &sr = NULL_RECT ) = 0;
+			virtual void drawImageStreched ( ImageObject &image, const Rect &dr, const Rect &sr = NULL_RECT ) = 0;
 
-	// Round - round to nearest integer
-	// Chille OK
-	double round(double val)
-	{
-		return ipart(val + 0.5);
-	}
+			virtual void fillRect ( const Rect &r, const Color &color ) = 0;
 
-	// Wtf is this?
-	double rfpart(double val)
-	{
-		return (1.0 - fpart(val));
-	}
+			virtual void outText ( const wstring &text, Font &font, int left, int top, const Color &color ) = 0;
 
-	virtual void line ( double x1, double y1, double x2, double y2, const Color &color );
+			virtual void putPixel ( int left, int top, const Color &color ) = 0;
+			virtual Color getPixel( const int& x, const int& y ) = 0;
+			virtual void hLine ( int left, int top, int width, const Color &color ) = 0;
+			virtual void vLine ( int left, int top, int height, const Color &color ) = 0;
 
-  virtual void pushClipRect ( const Rect &r ) = 0;
-  virtual Rect popClipRect (  ) = 0;
-  virtual Rect clipRect (  ) = 0;
+			/**
+			 * Integer part of val.
+			 *
+			 * @todo move to some utilty functions class.
+			 */
+			double ipart( double val ) {
+				int tmp = ( int )val;
+				return ( double )tmp;
+			}
 
-  virtual void setRelativePoint( const int left = 0, const int top = 0 ) = 0;
-  virtual void getRelativePoint( int& left, int& top ) = 0;
+			/**
+			 * Fractional part of val.
+			 *
+			 * @todo move to some utilty functions class.
+			 */
+			double fpart( double val ) {The GameUI network graph
+				int tmp = ( int )val;
+				return ( val-( double )tmp );
+			}
 
-  virtual void freeImage() = 0;
-  virtual bool isLoaded() = 0;
-  virtual bool loadImage( string fname ) = 0;
+			/**
+			 * Round to nearest integer.
+			 *
+			 * @todo move to some utilty functions class.
+			 */
+			double round( double val ) {
+				return ipart( val + 0.5 );
+			}
 
-  virtual int width(  ) = 0;
-  virtual int height(  ) = 0;
+			/**
+			 * WTF is this?
+			 *
+			 * @todo move to some utilty functions class.
+			 */
+			double rfpart( double val ) {
+				return ( 1.0 - fpart( val ) );
+			}
 
-  virtual int sectionWidth(  ) { return pSecWidth; }
-  virtual void setSectionWidht( const int& w ) { pSecWidth = w; }
-  virtual int sectionHeight(  ) { return pSecHeight; }
-  virtual void setSectionHeight( const int& h ) { pSecHeight = h; }
+			/**
+			 * Draw a line on this ImageObject.
+			 *
+			 * @see hLine() vLine().
+			 */
+			virtual void line ( double x1, double y1, double x2, double y2, const Color &color );
 
-	virtual Rect sectionRect( const int& index );
+			/**
+			 * Add a cliprect to limit where graphics can be drawn.
+			 *
+			 * @note The cliprect area cannot be made larger with this method, use popClipRect() for that.
+			 * @see popClipRect() clipRect().
+			 */
+			virtual void pushClipRect ( const Rect &r ) = 0;
 
-  virtual int numSections(  )
-  {
-    if ( ( pSecWidth <= 0 ) || ( pSecHeight <= 0 ) ) return 0;
-		return (int)floor( (double)(width() / pSecWidth) ) * (int)floor( (double)(height() / pSecHeight) );
-  }
+			/**
+			 * Remove last added cliprect.
+			 *
+			 * @see pushClipRect() clipRect().
+			 */
+			virtual Rect popClipRect (  ) = 0;
 
-	static ImageObject* createImageObject( const int& width = 0, const int& height = 0 )
-	{
-		if ( pImageObjectFactory != NULL ) {
-			return pImageObjectFactory( width, height );
-		} else {
-			return NULL;
-		}
-	}
+			/**
+			 * Return the current cliprect that limits where graphics can be drawn on this ImageObject.
+			 *
+			 * @see pushClipRect() popClipRect().
+			 */
+			virtual Rect clipRect (  ) = 0;
 
-	static ImageObjectFactory objectFactory()
-	{
-		return pImageObjectFactory;
-	}
+			virtual void setRelativePoint( const int left = 0, const int top = 0 ) = 0;
+			virtual void getRelativePoint( int& left, int& top ) = 0;
 
-	static void setObjectFactory( ImageObjectFactory imgf )
-	{
-		pImageObjectFactory = imgf;
-	}
+			virtual void freeImage() = 0;
+			virtual bool isLoaded() = 0;
+			virtual bool loadImage( string fname ) = 0;
 
-private:
+			virtual int width(  ) = 0;
+			virtual int height(  ) = 0;
 
-  int pSecWidth;
-  int pSecHeight;
-	static ImageObjectFactory pImageObjectFactory;
+			virtual int sectionWidth(  ) {
+				return pSecWidth;
+			}
+			virtual void setSectionWidht( const int& w ) {
+				pSecWidth = w;
+			}
+			virtual int sectionHeight(  ) {
+				return pSecHeight;
+			}
+			virtual void setSectionHeight( const int& h ) {
+				pSecHeight = h;
+			}
 
-};
+			virtual Rect sectionRect( const int& index );
+
+			virtual int numSections(  ) {
+				if ( ( pSecWidth <= 0 ) || ( pSecHeight <= 0 ) ) return 0;
+				return ( int )floor( ( double )( width() / pSecWidth ) ) * ( int )floor( ( double )( height() / pSecHeight ) );
+			}
+
+			static ImageObject* createImageObject( const int& width = 0, const int& height = 0 ) {
+				if ( pImageObjectFactory != NULL ) {
+					return pImageObjectFactory( width, height );
+				}
+				else {
+					return NULL;
+				}
+			}
+
+			static ImageObjectFactory objectFactory() {
+				return pImageObjectFactory;
+			}
+
+			static void setObjectFactory( ImageObjectFactory imgf ) {
+				pImageObjectFactory = imgf;
+			}
+
+		private:
+
+			int pSecWidth;
+			int pSecHeight;
+			static ImageObjectFactory pImageObjectFactory;
+
+	};
 
 }
 
